96896748f89a0bb03059330f0c6f6aa377382099
0x1eef <0x1eef@protonmail.com>
2025-10-25 08:09:17 +0000

    Call open(2) with OPEN_FLAGS

--- control/control.c
+++ control/control.c
@@ -28,6 +28,45 @@ sysdef_feature(hbsdctrl_ctx_t* ctx, const char* name, const char* path)
     return set_feature(ctx, name, path, -1);
 }
 
+int
+feature_status(hbsdctrl_ctx_t* ctx,
+               const char* name,
+               const char* path,
+               char** status)
+{
+    hbsdctrl_feature_t* feature;
+    hbsdctrl_feature_state_t state;
+    int res = 0, fd = -1;
+
+    errno = 0;
+    if (ctx == NULL) {
+        res = -1;
+        goto done;
+    }
+    if ((fd = open(path, OPEN_FLAGS)) == -1) {
+        res = -1;
+        goto done;
+    }
+    if ((feature = hbsdctrl_ctx_find_feature_by_name(ctx, name)) == NULL) {
+        res = -1;
+        goto done;
+    }
+    if (feature->hf_get(ctx, feature, &fd, &state) == RES_FAIL) {
+        res = -1;
+        goto done;
+    } else {
+        errno = 0;
+    }
+    *status = (char*)hbsdctrl_feature_state_to_string(&state);
+    goto done;
+done:
+    if (fd != -1)
+        close(fd);
+    if (errno != 0)
+        return errno;
+    return res;
+}
+
 static int
 set_feature(hbsdctrl_ctx_t* ctx, const char* name, const char* path, int state)
 {
@@ -60,45 +99,6 @@ set_feature(hbsdctrl_ctx_t* ctx, const char* name, const char* path, int state)
             goto done;
         }
     }
-    goto done;
-done:
-    if (fd != -1)
-        close(fd);
-    if (errno != 0)
-        return errno;
-    return res;
-}
-
-int
-feature_status(hbsdctrl_ctx_t* ctx,
-               const char* name,
-               const char* path,
-               char** status)
-{
-    hbsdctrl_feature_t* feature;
-    hbsdctrl_feature_state_t state;
-    int res = 0, fd = -1;
-
-    errno = 0;
-    if (ctx == NULL) {
-        res = -1;
-        goto done;
-    }
-    if ((fd = open(path, O_PATH)) == -1) {
-        res = -1;
-        goto done;
-    }
-    if ((feature = hbsdctrl_ctx_find_feature_by_name(ctx, name)) == NULL) {
-        res = -1;
-        goto done;
-    }
-    if (feature->hf_get(ctx, feature, &fd, &state) == RES_FAIL) {
-        res = -1;
-        goto done;
-    } else {
-        errno = 0;
-    }
-    *status = (char*)hbsdctrl_feature_state_to_string(&state);
     goto done;
 done:
     if (fd != -1)
